6ce820c0854ff1db63b369fb378d3016c8ec7965
Nancy <belscher@Nancys-MacBook-Pro.local>
2017-09-20 11:02:47 -0400

    simplified ace handling working

--- lib/hand.rb
+++ lib/hand.rb
@@ -14,34 +14,37 @@ end
 
 def calculate_score(hand)
   @hand_value = 0
+  @ace_count = 0
   hand.cards.each do |card|
     if card.value == "A"
       @ace_count += 1
-      @ace_is_eleven = true if @ace_count == 1
-      check_ace
+      @hand_value +=1
     elsif (card.value == "J" || card.value == "Q" || card.value == "K")
       @hand_value += 10
     else
       @hand_value += card.value
     end # if stmt
   end # do
-  if @hand_value > 21 && @ace_is_eleven
-    @hand_value -= 10
-    @ace_is_eleven = false
+  if @ace_count > 0
+    if (@hand_value + 10) > 21
+      # do nothing because making one Ace 11 would bust
+    else
+      @hand_value += 10
+    end
   end
 
   return @hand_value
 end # calculate_score
 
-def check_ace
-  if @ace_count == 1 && ((@hand_value + 11) > 21)
-    @hand_value += 1
-  elsif @ace_count > 1
-    @hand_value += 1
-  else
-    @hand_value += 11
-  end # if stmt
-
-end # check_ace
+# def check_ace
+#   if @ace_count == 1 && ((@hand_value + 11) > 21)
+#     @hand_value += 1
+#   elsif @ace_count > 1
+#     @hand_value += 1
+#   else
+#     @hand_value += 11
+#   end # if stmt
+#
+# end # check_ace
 
 end #class
